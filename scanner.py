fe8d613cfe3e3502e93d7503fc56e6ecac22a711
sivasweatha <u.sivasweatha@gmail.com>
2023-05-26 19:51:39 +0530

    Added scanner for trading

--- scanner.py
+++ scanner.py
@@ -1,16 +1,21 @@
 import yfinance as yf
 import csv
 
-symbols = []
+class Scanner:
+    def near_20_EMA():
+        symbols = []
 
-with open('stocks.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        symbol = row['Symbol']
-        try:
-            data = yf.download(symbol, period="1d", interval="1d", progress=False)
-            daily_return = data['Close'] + data['Open']
-            symbols.append(daily_return)
-        except Exception as e:
-            print(f"Error downloading data for {symbol}: {e}")
-print(symbols)
+        with open('stocks.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                symbol = row['Symbol']
+                try:
+                    data = yf.download(symbol, period="30d", interval="1d", progress=False)
+                    ema_20 = data['Close'].ewm(span=20, adjust=False).mean()
+                    if ema_20[-1] - 10 < data['Close'][-1] > ema_20[-1] + 10 :
+                        symbols.append(symbol)
+                except Exception as e:
+                    print(f"Error downloading data for {symbol}: {e}")
+
+scanner = Scanner()
+print(scanner.near_20_EMA())
